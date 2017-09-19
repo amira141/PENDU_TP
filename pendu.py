0ed47abdeced7878b6de80bb02cc4256555e951e
Amira <amira141592@gmail.com>
2017-09-19 16:09:35 +0300

    choix du mot par l'ordinateur au hasard dans une liste

--- pendu.py
+++ pendu.py
@@ -6,18 +6,36 @@ import os
 """***Le score commence à 0 et ne prend pas en charge de profil utilisateur***"""
 
 #Modules à importer spécifiques au jeu
-import random #Permet de choisir un mot au hasard STR dans une LISTE 
+from random import choice #Permet de choisir un mot au hasard STR dans une LISTE 
 
-#Début de partie : déclarer les variables
+#DEBUT DE PARTIE : DECLARER LES VARIABLES 
+
+"""score : le score de l'utilisateur 
+liste_pendu_8 : liste des mots de 8 lettres dans laquelle l'ordinateur va en choisir un a faire deviner.
+mot_pendu : mot choisi dans la liste par l'ordinateur pour le faire deviner à l'utilisateur"""
+
 
 score = 0
+score = int(score)
+
 continuer_partie = True 
 
 print(" Bienvenue sur le jeu du pendu :) ")
 
-#Le jeu 
+###LE JEU 
 
-#Fin de la partie 
+#-Choix du mot par l'ordinateur 
+
+liste_pendu_8 = ["poulet", "bonbon", "haricot", "patate"] #c
+mot_pendu = choice(liste_pendu)
+
+
+#-L'utilisateur devine
+
+
+
+
+#FIN DE LA PARTIE 
 
 
 
