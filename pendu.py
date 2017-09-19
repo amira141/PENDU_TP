97bdcee01ac2f0a3549d1db697f92e40e50f5605
Amira <amira141592@gmail.com>
2017-09-19 18:15:15 +0300

    propose a lutilisateur de saisir une lettre, incrémentation de 8 tours

--- pendu.py
+++ pendu.py
@@ -13,7 +13,8 @@ from random import choice #Permet de choisir un mot au hasard STR dans une LISTE
 """score : le score de l'utilisateur 
 liste_pendu_8 : liste des mots de 8 lettres dans laquelle l'ordinateur va en choisir un a faire deviner.
 mot_pendu : mot choisi dans la liste par l'ordinateur pour le faire deviner à l'utilisateur
-lettres_etoile = il s'agit des lettres du mots à deviner transformées en étoiles """
+lettres_etoile : il s'agit des lettres du mots à deviner transformées en étoiles 
+deviner_lettre : """
 
 
 score = 0
@@ -27,24 +28,39 @@ print(" Bienvenue sur le jeu du pendu :) ")
 
 #-Choix du mot par l'ordinateur 
 
-liste_pendu_8 = ["poulet", "bonbon", "haricot", "patate"] 
-mot_pendu = choice(liste_pendu)
+while continuer_partie: 
 
+    liste_pendu_8 = ["poulet", "bonbon", "haricot", "patate"] 
+    mot_pendu = choice(liste_pendu_8)
 
-for lettres in mot_pendu :
-	lettres_etoile = str.maketrans(lettres,"*")
-	print("Voici le mot à deviner:",lettres.translate(lettres_etoile))
+    print(mot_pendu) #Essais 
+    
+    print("Voici le mot à deviner:")
+    
+    for lettres in mot_pendu :
+        lettres_etoile = str.maketrans(lettres,"*")
+        print(lettres.translate(lettres_etoile))
 
 
 #-L'utilisateur devine
 
+    i = 1 
+    while i<9 :
+        deviner_lettre = input("Veuillez saisir une lettre")
+        i += 1
+
+        if len(deviner_lettre) >= 2:
+            print("Il n'est pas demandé d'écrire un mot.")
+            i = 9-i
+
+        
+
+
+      
 
 
 
 #FIN DE LA PARTIE 
-
-
-
 
 
 os.system("pause")
