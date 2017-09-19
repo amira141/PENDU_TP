9622dcf7e5ae551c1cc89edc8d01a56d71b5a3f7
Amira <amira141592@gmail.com>
2017-09-19 15:47:21 +0300

    Création du squelette du programme simplifié du jeu du pendu.

---
+++ pendu.py
@@ -0,0 +1,26 @@
+from encodings import utf_8
+import os 
+
+"""***JEU DU PENDU SIMPLE***"""
+"""***Cette version du pendu est simplifiée.***"""
+"""***Le score commence à 0 et ne prend pas en charge de profil utilisateur***"""
+
+#Modules à importer spécifiques au jeu
+import random #Permet de choisir un mot au hasard STR dans une LISTE 
+
+#Début de partie : déclarer les variables
+
+score = 0
+continuer_partie = True 
+
+print(" Bienvenue sur le jeu du pendu :) ")
+
+#Le jeu 
+
+#Fin de la partie 
+
+
+
+
+
+os.system("pause")
