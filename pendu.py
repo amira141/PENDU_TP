33a112a271b9f80b0c836f4c3efe796c51f754d4
Amira <amira141592@gmail.com>
2017-09-20 18:44:55 +0300

    Corps d'un pendu simplifié sans score et sans profil utilisateur qui fonctionne

--- pendu.py
+++ pendu.py
@@ -1,65 +1,56 @@
 from encodings import utf_8
-import os 
+import os
 
-"""***JEU DU PENDU SIMPLE***"""
+
+"""***JEU DU PENDU SIMPLIFIE***"""
 """***Cette version du pendu est simplifiée.***"""
-"""***Le score commence à 0 et ne prend pas en charge de profil utilisateur***"""
+"""***Il n'y a pas de score et pas de profil utilisateur***"""
 
-"""module spécifique au jeu"""
-from random import choice 
-
-#LES VARIABLES 
-
-"""
-liste_pendu_8 : liste des mots de 8 lettres dans laquelle l'ordinateur va en choisir un a faire deviner.
-
-tours : il s'agit du nombre de chances autorisées 
-
-mot_pendu : mot choisi dans la liste par l'ordinateur pour le faire deviner à l'utilisateur
-
-lettres_etoile : il s'agit des lettres du mots à deviner transformées en étoiles
-
-L : il s'agit d'une variable représentant le mot modifié par les étoiles 
-
-lettres : il s'agit des lettres du mot choisi par l'ordinateur 
-
- """
+"""module spécifique au jeu """
+from random import choice
 
 
-liste_pendu_8 = ["pas", "bonbon", "mama"]
-tours = 8 
+#DECLARATION DES VARIABLES
 
-#AFFICHAGE D'UN MOT PAR L'ORDINATEUR 
 
-mot_pendu = choice(liste_pendu_8)
-print(mot_pendu) 
+liste_pendu = ["poulet", "bonbon", "maman"]
+reservoir = []
 
+
+#AFFICHAGE DU MOT PAR LORDINATEUR 
+
+print("Bienvenue sur notre pendu en ligne.")
+
+mot_pendu = choice(liste_pendu)
+print("TEST DU MOT",mot_pendu)
 print("Voici le mot à deviner:")
 
 for lettres in mot_pendu :
-        lettres_etoile = str.maketrans(lettres,"*")
-        print(lettres.translate(lettres_etoile))
+    lettres_etoile = str.maketrans(lettres,"*")
+    print(lettres.translate(lettres_etoile))
+    
 
+#JEU 
 
-#MOT MODIFIE PAR LES ETOILES
-L = ""
+i = 0
+i = int(i)
 
-lettre_u = input("Veuillez saisir une lettre ")
-
-for lettres in mot_pendu :
-    if lettres in lettre_u:
-        L = L + lettres
-    else:
-        L= L + "*"
- 
-print(L) 
-
-#PARTIE DE PENDUE
-
-
-
-
-
+while i<8 :
+    lettre_u = input("Devinez une lettre du mot.")
+    
+    reservoir.append(lettre_u)
+    print("Les lettres que vous avez déjà tapé sont:",reservoir)
+    
+    for lettre_r in reservoir :
+        if lettre_r == lettres in mot_pendu :
+            print("Bravo, vous avez gagné!")
+    i = i + 1 
+    print("Il vous reste",(8- i), "essais")
+    
+else:
+    print("Désolé, vous avez perdu! Bye!") 
+    
+    
 
 
 
